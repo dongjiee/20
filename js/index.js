d871e40f00298415984735ee150e06d67e138582
dongjiee <dongjie216@icloud.com>
2023-07-31 02:17:23 +0800

    new

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 window.onload=function()
 {
+    var page=document.getElementsByClassName('page')
     var box=document.getElementById('box')
-    var img=document.getElementsByClassName('img')
-    var back=document.getElementsByClassName('back')
-    // var audio=new Audio('asset/birthday.mp3')
+    var audio=new Audio('asset/birthday.mp3')
 
+    var cur=page.length
     var click=false
     var s=false
 
@@ -15,80 +15,59 @@ window.onload=function()
         if(document.body.clientHeight*0.45+50>document.body.clientWidth)
         {
             box.style.width=document.body.clientHeight*0.45-50+'px'
-            box.style.height=box.style.width
         }
         else
         {
             box.style.width=document.body.clientHeight*0.45+'px'
-            box.style.height=box.style.width
         }
     }
 
-    for(var i=0;i<img.length;i++)
+    for(var i=0;i<page.length;i++)
 	{	
-		img[i].index=i;
+		page[i].index=i;
 
-		img[i].onclick=function()
+		page[i].onclick=function()
 		{	
-            if(!click)
+            if(click==false)
             {
-                click=true
+                num=this.index;
+
+                if(cur>=num+1)
+                {
+                    page[num].classList.remove('pre')
+                    page[num].classList.add('next')
+                    cur=num
+
+                    setTimeout(function(){page[num].style.zIndex=10-num},1000);
+                    
+                    if(s==false)
+                    {
+                        song()
+                        s=true
+                    }
+                }
+                else
+                {
+                    page[num].classList.remove('next')
+                    page[num].classList.add('pre')
+                    cur=num+1
+                    
+                    setTimeout(function(){page[num].style.zIndex=num+1},1000);
+                }
+
                 clear()
-
-                // if(!s)
-                // {
-                //     song()
-                //     s=true
-                // }
-                index=this.index
-
-                // flip_page(this.index,1)
-                img[index].classList.remove('pre')
-                back[index].classList.remove('pre')
-                img[index].classList.add('next')
-                back[index].classList.add('next')
-
-                // setTimeout(function(){back[index].style.zIndex=10-index+1;img[index].style.zIndex=10-index},1000)
-                setTimeout(function(){back[index].style.zIndex=-index},1000)
-                
+                click=true
             }
 		}
 	}
-
-    for(var j=0;j<back.length;j++)
-    {
-        back[j].index=j;
-
-		back[j].onclick=function()
-        {
-            if(!click)
-            {
-                click=true
-                clear()
-
-                index=this.index
-
-                // flip_page(this.index,2)
-
-                back[index].classList.remove('next')
-                img[index].classList.remove('next')
-                back[index].classList.add('pre')
-                img[index].classList.add('pre')
-
-                // setTimeout(function(){back[index].style.zIndex=10+index;img[index].style.zIndex=10+index+1},1000)
-                // setTimeout(function(){back[index].style.zIndex=-(index+1)},1000)
-            }
-            
-        }
-    }
 
     function clear()
     {
         setTimeout(function(){click=false},2000);
     }
 
-    // function song()
-    // {
-    //     setTimeout(function(){audio.play()},800);
-    // }
+    function song()
+    {
+        setTimeout(function(){audio.play()},800);
+    }
 }
